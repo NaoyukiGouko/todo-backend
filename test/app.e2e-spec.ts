a54199bc125b9e6a1fe187e6780a7d1d1e73c6e3
郷古直幸 <naoyukigoko@goukonaoyukinoMacBook-Pro.local>
2020-07-16 17:13:54 +0900

    ToDoControllerのe2eテストを追加する #5

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-//import { AppModule } from './../src/app.module';
 import { TodoModule } from './../src/to-do/to-do/to-do.module';
-//import { TodoService } from './../src/to-do/to-do/to-do.service';
 import { Todo } from './../src/to-do/to-do';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('Todo', () => {
   let app: INestApplication;
-  //const todos: ToDo[] = [];
-  //const todo: ToDo = null;
-  //const i: number = null;
-  //const todoService = {findAll: () => ['test'], delete: () => ['test'], create: () => ['test']};
+
+  const cre = {
+    query: {
+      title: test
+    }
+  };
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -43,36 +43,18 @@ describe('Todo', () => {
   it('/POST create', () => {
     return request(app.getHttpServer())
       .post('/create')
+      .type('json')
+      .send({title: "test"})
       .expect(201)
   });
 
   it('/DELETE :id', () => {
     return request(app.getHttpServer())
-      .delete('/:id')
+      .delete('/7')
       .expect(200)
   });
 
   afterEach(async () => {
     await app.close();
   });
-  /**
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-  */
-
-
 });
